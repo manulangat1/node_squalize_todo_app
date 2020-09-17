1b2198cfb1c27b9754b655604e04e5e44e5dc95e
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-17 19:44:42 +0300

    rf:refactoring_our_models_and_adding_neccessary_fields:backend

---
+++ server/models/todoitem.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TodoItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      TodoItem.associate = (models) => {
+        TodoItem.belongsTo(models.Todo, {
+          foreignKey: 'todoId',
+          onDelete: 'CASCADE',
+        });
+      };
+    }
+  };
+  TodoItem.init({
+    content:{
+      type:DataTypes.STRING,
+      allowNull:false
+
+    } ,
+    complete:{
+      type:DataTypes.BOOLEAN,
+      defaultValue:false
+    } 
+  }, {
+    sequelize,
+    modelName: 'TodoItem',
+  });
+  return TodoItem;
+};
