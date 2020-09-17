31543fcf9714fcd086d49520266fb6e9ffefb07b
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-18 00:22:34 +0300

    bg:hit_a_stupid_db_bug:backned

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV;
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
@@ -12,8 +12,10 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log(`${config}`)
+  sequelize = new Sequelize(`postgres://manulangat:3050manu@127.0.0.1:5432/dev_db`,config);
 }
+
 
 fs
   .readdirSync(__dirname)
