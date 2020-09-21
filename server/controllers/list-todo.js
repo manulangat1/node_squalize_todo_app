7162d641f2ecab9d767f70987042f327786d0c39
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-21 18:05:05 +0300

    ft:get_by_title:backend

--- server/controllers/list-todo.js
+++ server/controllers/list-todo.js
@@ -3,17 +3,13 @@ const Todo = db.Todo
 
 
 exports.listd = async(req,res) => {
-    console.log("Helloe")
-    console.log(`${Todo}`)
-    try{
-        
+    try{       
         const todo = await Todo.findAll()
         return res.status(200).json({
             success:true,
             data:todo
         })
     } catch(err){
-        console.log(err)
         return res.status(500).json({
             success:false,
             message:err
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const {creates} = require('../controllers/todo')
 const {listd} = require('../controllers/list-todo')
+const { getByName } = require('../controllers/check-todo')
 
 
 const router = express.Router()
 
 
 router.route('/').get(listd).post(creates)
+router.route('/find/').get(getByName)
 
 module.exports = router
