a88f1a6f3b4c559a8774dc78ac672ec40cd6c938
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-21 17:29:49 +0300

    ft:adding_list_type:backend

--- server/controllers/list-todo.js
+++ server/controllers/list-todo.js
@@ -1,16 +1,19 @@
-const db = require('../models/index')
-const Todo = db.Todo 
+const db  = require('../models/index')
+const Todo = db.Todo
 
 
 exports.listd = async(req,res) => {
     console.log("Helloe")
+    console.log(`${Todo}`)
     try{
-        const todo = await todo.list()
+        
+        const todo = await Todo.findAll()
         return res.status(200).json({
             success:true,
             data:todo
         })
     } catch(err){
+        console.log(err)
         return res.status(500).json({
             success:false,
             message:err
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {creates} = require('../controllers/todo')
+const {listd} = require('../controllers/list-todo')
 
 
 const router = express.Router()
 
 
-router.route('/').post(creates)
+router.route('/').get(listd).post(creates)
 
 module.exports = router
